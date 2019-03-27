1f2484c3e681e44dd95700d372ee2b90e0ae0f32
George Appiah <george.appiah@gmail.com>
2019-03-27 05:15:49 +0000

    Initial Genesis child theme setup

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Theme customizations
+ *
+ * @package      BorgaPress
+ * @author       George Appiah
+ * @link         https://github.com/georgeappiha/borgapress
+ * @copyright    Copyright (c) 2019, George Appiah
+ * @license      GPL-3.0
+ */
+add_action( 'genesis_setup', 'custom_setup',15);
